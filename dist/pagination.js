b822c90c17ae7a8edb7e1f4d6e8f4ad44352c75f
marcin0115 <marcin0115@op.pl>
2022-09-20 19:59:52 +0200

    change commit

--- dist/pagination.js
+++ dist/pagination.js
@@ -1,9 +1,18 @@
-const data = [1, 2, 3, 4, 5, 6];
+// 1) Stwórz funkcję paginateArray, która przyjmuje jako 1 argument tablicę, a jako 2 argument obiekt settings z następującymi kluczami :
+//   a) „actualPageIndex” - numer strony.
+//   b) „entriesOnPage” – ilośc obiektów na pojedynczej stronie.
+//2) Funkcja zwraca entriesOnSelectedPage, który jest arrayem podzielonym według ustawień z settings.
+// + interface
 const settings = { actualPageIdx: 2, entriesOnPage: 2 };
-const paginateArray = (dataEntries, { actualPageIdx, entriesOnPage } = settings) => {
+// + typ
+const data = [1, 2, 3, 4, 5, 6];
+//+ interface
+const paginateArray = (dataEntries, { actualPageIdx, entriesOnPage }) => {
     const start = actualPageIdx * entriesOnPage - entriesOnPage;
     const end = start + entriesOnPage;
+    // + typ
     const entriesOnSelectedPage = dataEntries.slice(start, end);
     return entriesOnSelectedPage;
 };
+// + typ
 const result = paginateArray(data, settings);
--- src/pagination.ts
+++ src/pagination.ts
@@ -1,14 +1,29 @@
-const data = [1, 2, 3, 4, 5, 6];
-const settings = { actualPageIdx: 2, entriesOnPage: 2 };
+// 1) Stwórz funkcję paginateArray, która przyjmuje jako 1 argument tablicę, a jako 2 argument obiekt settings z następującymi kluczami :
+//   a) „actualPageIndex” - numer strony.
+//   b) „entriesOnPage” – ilośc obiektów na pojedynczej stronie.
+//2) Funkcja zwraca entriesOnSelectedPage, który jest arrayem podzielonym według ustawień z settings.
 
+//Interface
+interface Sett {
+  actualPageIdx: number;
+  entriesOnPage: number;
+}
+// + interface
+const settings: Sett = { actualPageIdx: 2, entriesOnPage: 2 };
+// + typ
+const data: number[] = [1, 2, 3, 4, 5, 6];
+
+//+ interface
 const paginateArray = (
   dataEntries: number[],
-  { actualPageIdx, entriesOnPage } = settings
+  { actualPageIdx, entriesOnPage }: Sett
 ) => {
   const start = actualPageIdx * entriesOnPage - entriesOnPage;
   const end = start + entriesOnPage;
 
-  const entriesOnSelectedPage = dataEntries.slice(start, end);
+  // + typ
+  const entriesOnSelectedPage: number[] = dataEntries.slice(start, end);
   return entriesOnSelectedPage;
 };
-const result = paginateArray(data, settings);
+// + typ
+const result: number[] = paginateArray(data, settings);
